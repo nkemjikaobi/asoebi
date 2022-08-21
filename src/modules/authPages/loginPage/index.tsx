244c16269a430289522f6c8dffd34519759d06b9
Obi Nkemjika Derick <obinkemjikaderick@Obis-MacBook-Pro.local>
2022-08-21 14:31:59 +0100

    testing workflow

--- src/modules/authPages/loginPage/index.tsx
+++ src/modules/authPages/loginPage/index.tsx
@@ -1,6 +1,8 @@
-import React, { FC } from "react";
 
 import Login from "@components/organisms/AuthenticationPages/Login/Login";
+
+import React, { FC } from "react";
+
 
 export const LoginPage: FC = () => (
   <>
