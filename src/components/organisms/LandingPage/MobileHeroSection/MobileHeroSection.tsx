8e0903c41cb2f9fe673da401c6292703e3f9966d
Obi Nkemjika Derick <obinkemjikaderick@Obis-MacBook-Pro.local>
2023-01-19 15:19:08 +0100

    Responsivness for home et al

--- src/components/organisms/LandingPage/MobileHeroSection/MobileHeroSection.tsx
+++ src/components/organisms/LandingPage/MobileHeroSection/MobileHeroSection.tsx
@@ -5,12 +5,10 @@ import CustomButton from "@components/atoms/CustomButton/CustomButton";
 
 import { ButtonProperties } from "@shared/libs/helpers";
 
-import AsoebiLand from "@images/png/asoebi-land.png";
-
 const MobileHeroSection = () => {
   return (
     <div className="relative smallLaptop:hidden">
-      <Image height={636} src={AsoebiLand} width={375} />
+      <Image height={636} src="/images/png/aosebi-land.png" width={375} />
       <div className="absolute bottom-0 w-[23.438rem] h-[16.125rem] bg-asoebiWhite-200 pt-[1.875rem] px-[1.063rem] pb-[1.313rem]">
         <h3 className="text-24 font-medium text-asoebiBlack-900">We do the work for you @Aso Ebi</h3>
         <p className="text-14 mt-2 mb-4 text-asoebiGray-202">
